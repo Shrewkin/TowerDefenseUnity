0a74dc2a18055774a3d605f2d2c242e24a6e1f1f
Shrewkin <strizhevp@gmail.com>
2018-11-06 01:15:33 -0800

    Waves
    Finished wave spawning and path finding was adjusted and tweaked

--- Assets/Scripts/EnemyScript.cs
+++ Assets/Scripts/EnemyScript.cs
@@ -16,7 +16,7 @@ public class EnemyScript : MonoBehaviour
         Vector3 direction = target.position - transform.position;
         transform.Translate(direction.normalized * speed * Time.deltaTime, Space.World);
 
-        if (Vector3.Distance(transform.position, target.position) <= 1.0f)
+        if (Vector3.Distance(transform.position, target.position) <= 0.4f)
         {
             GetNextWaypoint();
         }
---
+++ Assets/Scripts/WavesSpawner.cs
@@ -0,0 +1,44 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class WavesSpawner : MonoBehaviour
+{
+    public Transform enemyPrefab;
+
+    public Transform spawnPoint;
+
+    public float timeBetweenWaves = 5.0f;
+    public float timeBetweenSpawn = 0.5f;
+    public Text waveCountdownTimer;
+
+    private float waveIndex = 0;
+    private float countdown = 2.0f;
+
+    void Update()
+    {
+        if (countdown <= 0)
+        {
+            StartCoroutine(SpawnWave());
+            countdown = timeBetweenWaves;
+        }
+
+        countdown -= Time.deltaTime;
+        waveCountdownTimer.text = Mathf.Round(countdown).ToString();
+    }
+
+    IEnumerator SpawnWave()
+    {
+        waveIndex++;
+        for (int i = 0; i < waveIndex; i++)
+        {
+            SpawnEnemy();
+            yield return new WaitForSeconds(timeBetweenSpawn);
+        }
+    }
+
+    void SpawnEnemy()
+    {
+        Instantiate(enemyPrefab, spawnPoint.position, spawnPoint.rotation);
+    }
+}
